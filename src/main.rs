c4eeed86a404a5be3f6d9ee8cd75f0da8b220d0d
pseudobabble <harryjohnson500@gmail.com>
2023-08-12 11:54:05 +0100

    trying out rust-peg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::any::Any;
+use std::ops::Add;
+use std::rc::Rc;
 
 extern crate uom;
 
@@ -14,39 +16,143 @@ use uom::si::time::second;
 ///  * ((proportion_proxy_benefit_attributed_to_wellbeing_anxiety[float] + proportion_proxy_benefit_attributed_to_wellbeing_depression[float]) / 2)
 ///  * imp_employee_time_in_mental_wellbeing_programmes[time]
 
-struct Value {
-    name: str,
+/// average_wage[float] = total_wage[float] / number_of_employees[int]
+/// average_wage_per_unit_calendar_time =
+
+#[derive(Debug)]
+enum Operator {
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    Identity,
+}
+
+#[derive(Debug)]
+pub struct Variable {
+    name: String,
+    value: f32,
+}
+
+impl Variable {
+    fn to_expression(self) -> Expression<Variable> {
+        Expression {
+            lhs: None,
+            rhs: self,
+            operator: Operator::Identity,
+        }
+    }
+}
+
+impl Add<Variable> for Variable {
+    type Output = Expression<Variable>;
+
+    fn add(self, _rhs: Variable) -> Expression<Variable> {
+        Expression {
+            lhs: Some(self),
+            rhs: _rhs,
+            operator: Operator::Add,
+        }
+    }
+}
+
+impl Add<Expression<Variable>> for Variable {
+    type Output = Expression<Variable>;
+
+    fn add(self, _rhs: Expression<Variable>) -> Expression<Variable> {
+        Expression {
+            lhs: Some(Variable {
+                name: "_".to_string(),
+                value: self.value + _rhs.rhs.value,
+            }),
+            rhs: _rhs.rhs,
+            operator: Operator::Add,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Expression<T> {
+    lhs: Option<T>,
+    rhs: T,
+    operator: Operator,
+}
+
+impl Add for Expression<Variable> {
+    type Output = Expression<Variable>;
+
+    fn add(self, _rhs: Expression<Variable>) -> Expression<Variable> {
+        let rhs_lhs_value = match _rhs.lhs {
+            Some(value) => value.value,
+            None => 0.0,
+        };
+        Expression {
+            lhs: Some(Variable {
+                name: "_".to_string(),
+                value: self.rhs.value + rhs_lhs_value,
+            }),
+            rhs: _rhs.rhs,
+            operator: Operator::Add,
+        }
+    }
 }
 
 peg::parser! {
+
     grammar route2_parser() for str {
 
-        rule number -> f64
-            =n:$(['.'|'0'..='9']+) { n.parse::<f64>().unwrap() }
+        rule number() -> f32
+            = n:$(['.'|'0'..='9']+) { n.parse::<f32>().unwrap() }
 
-        rule variable -> Value
-            = n:$(['a'..='z'|'A'..='Z'|'0'..='9'|'-']+) { n.into() }
+        // -1 etc
+        // rule unary(num: rule<str>) -> f32
+        //     = n:$(['-'num()]) { n.parse::<f32>().unwrap() * -1.0 }
 
-        rule whitepace
+        rule label() -> String
+            = v:$(['a'..='z'|'A'..='Z'|'0'..='9'|'_']*) { v.into() }
+
+        rule variable() -> Variable
+            = v:label() _ "=" _ n:number() {
+                Variable {
+                    name: v,
+                    value: n
+                }
+            }
+
+        pub rule variables() -> Vec<Variable>
+            = v:variable() ++ "; " { v }
+
+        rule expression() -> Expression<Variable>
+            = "(" _ e:expression() _ ")" { e }
+        // whitespace
+        rule _
             = [' '|'\n']*
 
-        pub rule arithmetic() -> f64 = precedence!{
-            x:(@) "+" y:@ { x + y }
-            x:(@) "-" y:@ { x - y }
-            --
-            x:(@) "*" y:@ { x * y }
-            x:(@) "/" y:@ { x / y }
-            --
-            x:@ "^" y:(@) { x.pow(y as u32) }
-            --
-            n:number() { n }
-            "(" e:arithmetic() ")" { e }
-        }
+        // pub rule parse() -> Vec<Variable> = precedence! {
+        //     x:(@) _ "+" _ y:@ { x + y }
+        //     // x:(@) _ "-" _ y:@ { x - y }
+        //     // --
+        //     // x:(@) _ "*" _ y:@ { x * y }
+        //     // x:(@) _ "/" _ y:@ { x / y }
+        //     // --
+        //     // x:@ "^" y:(@) { f32::pow(x, y) }
+        //     // --
+        //     "(" _ e:parse() _ ")" { e }
+        //     --
+        //     // "-" x:(@) { x * -1.0}
+        //     variables:variable() ++ "; " { variables }
+
+        // }
 
   }
 }
 
 fn main() {
-    let x = 3.0 + 2;
-    println!("Hello, world!");
+    println!("{:#?}", route2_parser::variables("x = 3; y = 5"));
+    // println!("{:#?}", route2_parser::arithmetic("24.7 + 37.0"));
+    // println!("{:#?}", route2_parser::arithmetic("(24.7 + 37.0) / 2"));
+    // println!(
+    //     "{:#?}",
+    //     route2_parser::arithmetic("((24.7 + 37.0) / 2) * -1")
+    // );
 }
